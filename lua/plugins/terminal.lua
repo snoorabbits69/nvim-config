18bddf2676aed7fd373c06e3514181c8b44f5fbf
snoorabbits <roshanadhikari66666@gmail.com>
2026-03-09 13:41:47 +0000

    switched terminal with toggleterm

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,22 +1,43 @@
 return {
-"rebelot/terminal.nvim",
-config=function()
-require("terminal").setup({
-layout={open_cmd="botright new"}, -- for horizontal split
-cmd={vim.o.shell},
-autoclose=false,
-})
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      size = function(term)
+        if term.direction == "horizontal" then return 15
+        elseif term.direction == "vertical" then return vim.o.columns * 0.4
+        end
+      end,
+      direction     = "horizontal",
+      shell         = vim.o.shell,
+      close_on_exit = false,
+      autochdir     = true,
+      shade_terminals = true,
+      shading_factor  = 2,
+      start_in_insert = true,
+      persist_mode    = true,
+    })
 
---keymaps
-local term=require("terminal.mappings")
- vim.keymap.set("n", "<leader>tt", term.toggle, { desc = "Toggle Terminal" })
-    vim.keymap.set("n", "<leader>tr", term.run, { desc = "Run Command in Terminal" })
-    vim.keymap.set("n", "<leader>tk", term.kill, { desc = "Kill Terminal" })
-    vim.keymap.set("n", "<leader>tn", term.cycle_next, { desc = "Next Terminal" })
-    vim.keymap.set("n", "<leader>tp", term.cycle_prev, { desc = "Prev Terminal" })
+    -- ── Normal mode 
+    vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>",                    { desc = "Toggle Terminal" })
+    vim.keymap.set("n", "<leader>t1", "<cmd>1ToggleTerm<cr>",                   { desc = "Terminal 1" })
+    vim.keymap.set("n", "<leader>t2", "<cmd>2ToggleTerm<cr>",                   { desc = "Terminal 2" })
+    vim.keymap.set("n", "<leader>t3", "<cmd>3ToggleTerm<cr>",                   { desc = "Terminal 3" })
+    vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", { desc = "Vertical Terminal" })
+    vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>",    { desc = "Float Terminal" })
+    vim.keymap.set("n", "<leader>tr", function()
+      vim.ui.input({ prompt = "Run: " }, function(cmd)
+        if cmd and cmd ~= "" then
+          vim.cmd("TermExec cmd='" .. cmd .. "'")
+        end
+      end)
+    end, { desc = "Run Command in Terminal" })
 
-end
-
-
-
+    --Terminal
+    vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>",      { desc = "Exit Terminal Mode" })
+    vim.keymap.set("t", "<C-h>",      "<C-\\><C-n><C-w>h",{ desc = "Move Left" })
+    vim.keymap.set("t", "<C-j>",      "<C-\\><C-n><C-w>j",{ desc = "Move Down" })
+    vim.keymap.set("t", "<C-k>",      "<C-\\><C-n><C-w>k",{ desc = "Move Up" })
+    vim.keymap.set("t", "<C-l>",      "<C-\\><C-n><C-w>l",{ desc = "Move Right" })
+  end,
 }
